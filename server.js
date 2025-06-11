a2c85e103e13fd9a14e408187b29781232703f6f
binatone-bina <swabist0077@gmail.com>
2025-06-12 00:46:44 +0530

    trying again for cross domain issue(bina)

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ connectDB();
 
 app.use(cors({
     origin: ['https://health-chat-nexus.vercel.app', 'http://localhost:8080'],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
